3ff4ed1d081d7db2a41398ea2d9aaad4b15e3cbc
Guilherme Francisco <guisanfrancisco@gmail.com>
2026-04-30 09:52:15 -0300

    Streams: Arrays

--- java-26-streams-1/src/main/java/com/guisfco/Main.java
+++ java-26-streams-1/src/main/java/com/guisfco/Main.java
@@ -1,6 +1,7 @@
 package com.guisfco;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -8,6 +9,41 @@ public class Main {
     static void main() {
         IO.println("==== Lists ====");
         lists();
+
+        IO.println("\n==== Arrays ====");
+        arrays();
+    }
+
+    private static void arrays() {
+        // Arrays have a fixed size and it cannot be increased
+        var names = new String[]{"Guilherme", "Yasmin", "Kira"};
+
+        // Mutation
+        names[0] = "Pink";
+        IO.println(Arrays.toString(names));
+
+        // Iterating
+        for (var name : names) {
+            IO.println(name);
+        }
+
+        // When iterating an array using stream, use Arrays.stream(...) instead of Stream.of(...) to avoid issues with primitive types
+        var namesContainingA = Arrays.stream(names)
+                .filter(name -> name.contains("a"))
+                .count();
+
+        IO.println("Names containing A: " + namesContainingA);
+
+        // Converting primitive Array to List
+        var numbers = Arrays.stream(new int[]{1, 2, 3})
+                .boxed()
+                .toList();
+
+        IO.println("Values: " + numbers + ", Value type: " + numbers.getFirst().getClass());
+
+        // Multidimensional Arrays
+        var matrix = new int[][]{{1, 2, 3}, {3, 2, 1}, {0, 0, 0}};
+        IO.println("Matrix: " + Arrays.deepToString(matrix));
     }
 
     private static void lists() {
