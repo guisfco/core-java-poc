5d01b80ce84a4f6bb57d46d57a130d4cee920552
Guilherme Francisco <guisanfrancisco@gmail.com>
2026-05-21 09:56:50 -0300

    Annotations (Reflections): mini controller example

--- java-26-annotations-reflections/src/main/java/com/guisfco/Main.java
+++ java-26-annotations-reflections/src/main/java/com/guisfco/Main.java
@@ -2,6 +2,7 @@ package com.guisfco;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 
 public class Main {
 
@@ -19,7 +20,10 @@ public class Main {
             }
         }
 
-        var controllerMethods = ControllerExample.class.getDeclaredMethods();
+        // Registering routes
+        var controller = new ControllerExample();
+        var controllerMethods = controller.getClass().getDeclaredMethods();
+        var routes = new HashMap<String, Method>();
 
         for (Method method : controllerMethods) {
             var annotation = method.getAnnotation(GetMapping.class);
@@ -29,7 +33,27 @@ public class Main {
                 IO.println("Path: " + annotation.value());
                 IO.println("Enabled: " + annotation.enabled());
                 IO.println("Method name: " + method.getName());
+
+                if (annotation.enabled()) {
+                    routes.putIfAbsent(annotation.value(), method);
+                }
             }
+        }
+
+        IO.println("\n==== Registered routes ====");
+        IO.println(routes);
+
+        var request = new Request("/hello", null);
+        var route = routes.get(request.path());
+
+        IO.println("\n==== Call ====");
+        IO.println("Request: " + request);
+
+        if (route != null) {
+            var response = request.body() != null ? route.invoke(controller, request.body()) : route.invoke(controller);
+            IO.println("Response: " + response);
+        } else {
+            IO.println("Route not found");
         }
     }
 }
---
+++ java-26-annotations-reflections/src/main/java/com/guisfco/Request.java
@@ -0,0 +1,4 @@
+package com.guisfco;
+
+public record Request(String path, Object body) {
+}
